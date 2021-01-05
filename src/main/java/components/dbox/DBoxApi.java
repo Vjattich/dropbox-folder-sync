74d46eb082a45782cbc845ca3dfbf23112119879
Vjstupin <sektaaccel@gmail.com>
2021-01-06 03:47:42 +0500

    delete unused annotations

--- src/main/java/components/dbox/DBoxApi.java
+++ src/main/java/components/dbox/DBoxApi.java
@@ -1,7 +1,6 @@
 package components.dbox;
 
 import com.dropbox.core.v2.files.Metadata;
-import lombok.SneakyThrows;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -9,13 +8,10 @@ import java.util.List;
 
 public interface DBoxApi {
 
-    @SneakyThrows
     void upload(File file);
 
-    @SneakyThrows
     ByteArrayOutputStream download(String name);
 
-    @SneakyThrows
     List<Metadata> getFolderEntries();
 
 }
